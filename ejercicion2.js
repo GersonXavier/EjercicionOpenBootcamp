2102aafeaca2c02beeba2c3f837884999a596654
gerson luque <gersonjlh@gmail.com>
2022-12-08 10:57:01 -0500

    segundo commit

---
+++ ejercicion2.js
@@ -0,0 +1,13 @@
+let nombre = 'Gerson'
+let Edad = 26
+const isDeveloper = true
+const fechNac = new Date("aplil 06 1996");
+const libro = {
+    titulo: "Las 48 leyes del poder",
+    autor: "Robert Greene",
+    fecha: new Date(1998,1,1),  
+    url: "https://www.audible.com/pd/48-Laws-of-Power-Audiobook"
+
+}
+
+console.log(libro.fecha);
