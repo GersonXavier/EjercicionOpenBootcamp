5d25c2d3d95864e9844e40110e335bd206f6191a
gerson luque <gersonjlh@gmail.com>
2022-12-15 21:32:45 -0500

    Ejercicio4

---
+++ ejercicio4.js
@@ -0,0 +1,23 @@
+let nombre = 'Gerson';
+let apellido = 'Luque';
+let estudiante = nombre.concat(' ',apellido);
+
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let estudianteLong = estudiante.length;
+let primeNombre = nombre[0];
+let utiApellido = apellido[4];
+let elimEstudiante = estudiante.trim();
+let nomEstudiante = estudiante.includes('Gerson');
+
+
+console.log(nombre);
+console.log(apellido);
+console.log(estudiante);
+console.log(estudianteMayus);
+console.log(estudianteMinus);
+console.log(estudianteLong);
+console.log(primeNombre);
+console.log(utiApellido);
+console.log(elimEstudiante);
+console.log(nomEstudiante);;
